f1e523eb0b554a01cdb182eeb21dab0cc3e2c3e0
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-01-10 12:04:36 +0100

    added package version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Anime bot",
+    version='1.0.0',
+    author='GhoulKingR',
+    author_email='oduahchigozie46@gmail.com',
+    description='An anime bot to download your favourite animes',
+    url='https://github.com/GhoulKingR/anime-bot',
+    packages=find_packages(),
+    license='MIT'
+)
