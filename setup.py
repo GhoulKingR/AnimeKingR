bf003e913b82fe9dedc29669959a2763f86504f2
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-01-11 09:37:39 +0100

    converted to script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="Anime bot",
+    version='1.0.0',
+    author='GhoulKingR',
+    author_email='oduahchigozie46@gmail.com',
+    description='An anime bot to download your favourite animes',
+    url='https://github.com/GhoulKingR/anime-bot',
+    scripts=['./scripts/anime-bot'],
+    packages=['anime-bot'],
+    license='MIT',
+    install_requires=[
+        "requests == 2.31.0",
+        "selenium == 4.16.0",
+        "tqdm == 4.66.1",
+        "setuptools",
+    ],
+)
